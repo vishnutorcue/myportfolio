fe29ded705eb5eb484bdac9bd5d5de4d3e867f82
Vishnu vikram <vishnuvik7@gmail.com>
2023-12-27 12:55:39 +0530

    updated projects

--- src/components/Project/Project.tsx
+++ src/components/Project/Project.tsx
@@ -1,6 +1,4 @@
 import { Container } from "./styles";
-import githubIcon from "../../assets/github.svg"
-import DownloadApp from '../../assets/download.png'
 import externalLink from "../../assets/external-link.svg"
 import ScrollAnimation from "react-animate-on-scroll";
 
